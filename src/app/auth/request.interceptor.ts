47c1894bd6b330bf2c8f5e13e19ae4544a6bdfc4
Michel <michelfigueiratemp@gmail.com>
2021-11-03 17:40:55 -0300

    Lazy Loading in Auth

---
+++ src/app/auth/request.interceptor.ts
@@ -0,0 +1,23 @@
+import { Injectable } from "@angular/core";
+import { HttpHandler, HttpHeaderResponse, HttpInterceptor, HttpProgressEvent, HttpRequest, HttpResponse, HttpSentEvent, HttpUserEvent } from "@angular/common/http";
+
+import { Observable } from "rxjs";
+import { TokenService } from "../core/token/token.service";
+
+@Injectable()
+export class RequestInterceptor implements HttpInterceptor {
+
+    constructor(private tokenService: TokenService) {}
+
+    intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpSentEvent | HttpHeaderResponse | HttpProgressEvent | HttpResponse<any> | HttpUserEvent<any>> {
+      if(this.tokenService.hasToken()) {
+        const token = this.tokenService.getToken();
+        req = req.clone({
+          setHeaders: {
+            'x-access-token': token, 
+          }
+        });
+      }
+        return next.handle(req);
+    }
+}
